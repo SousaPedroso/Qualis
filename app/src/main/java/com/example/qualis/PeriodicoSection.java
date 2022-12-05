16eda19825505bcca092cf45278a80e47a7df171
SousaPedroso <gsousa844@gmail.com>
2022-12-04 22:29:54 -0400

    Initial layout for periodicos section

---
+++ app/src/main/java/com/example/qualis/PeriodicoSection.java
@@ -0,0 +1,14 @@
+package com.example.qualis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeriodicoSection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_periodico_section);
+    }
+}
