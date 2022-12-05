e9f07741b3540df1fc6839c75b16f1ff753d08ec
SousaPedroso <gsousa844@gmail.com>
2022-12-04 22:34:15 -0400

    Periodicos section through main page

--- app/src/main/java/com/example/qualis/MainActivity.java
+++ app/src/main/java/com/example/qualis/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.qualis;
 
+import static com.example.qualis.R.id.button_periodicos_section;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonPeriodicos = findViewById(button_periodicos_section);
+        buttonPeriodicos.setOnClickListener(this);
+        Button buttonConferencias = findViewById(R.id.button_conferencias_section);
+        buttonConferencias.setOnClickListener(this);
+        Button buttonCorrelacao = findViewById(R.id.button_correlacao_section);
+        buttonCorrelacao.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case button_periodicos_section:
+                Intent intent = new Intent(this, PeriodicoSection.class);
+                startActivity(intent);
+        }
     }
 }
