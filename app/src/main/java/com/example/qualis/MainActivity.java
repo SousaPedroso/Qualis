3ddcb277cb1209d9324081fbd6bc4ef03ee5b098
SousaPedroso <gsousa844@gmail.com>
2022-12-08 11:15:44 -0400

    Fixed transition between Main and Periodicos

--- app/src/main/java/com/example/qualis/MainActivity.java
+++ app/src/main/java/com/example/qualis/MainActivity.java
@@ -6,40 +6,29 @@ import static com.example.qualis.R.id.button_periodicos_section;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button buttonPeriodicos = findViewById(button_periodicos_section);
-        buttonPeriodicos.setOnClickListener(this);
-        Button buttonConferencias = findViewById(button_conferencias_section);
-        buttonConferencias.setOnClickListener(this);
-        Button buttonCorrelacao = findViewById(button_correlacao_section);
-        buttonCorrelacao.setOnClickListener(this);
-    }
+        buttonPeriodicos.setOnClickListener(view -> startActivity(
+                new Intent(MainActivity.this, PeriodicoSection.class))
+        );
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case button_periodicos_section:
-                Intent periodicosIntent = new Intent(this, PeriodicoSection.class);
-                startActivity(periodicosIntent);
-            case button_conferencias_section:
-                Intent conferenciasIntent = new Intent(this, ConferenciaSection.class);
-                startActivity(conferenciasIntent);
-            case button_correlacao_section:
-                Intent correlacaoIntent = new Intent(this, Correlacao_Section.class);
-                startActivity(correlacaoIntent);
-        }
+        Button buttonConferencias = findViewById(button_conferencias_section);
+        buttonConferencias.setOnClickListener(view -> startActivity(
+                new Intent(MainActivity.this, ConferenciaSection.class))
+        );
+
+        Button buttonCorrelacao = findViewById(button_correlacao_section);
+        buttonCorrelacao.setOnClickListener(view -> startActivity(
+                new Intent(MainActivity.this, ConferenciaSection.class))
+        );
     }
 }
--- app/src/main/java/com/example/qualis/PeriodicoSection.java
+++ app/src/main/java/com/example/qualis/PeriodicoSection.java
@@ -6,40 +6,29 @@ import static com.example.qualis.R.id.button_periodicos_section;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class PeriodicoSection extends AppCompatActivity implements View.OnClickListener{
-
+public class PeriodicoSection extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_periodico_section);
 
         Button buttonPeriodicos = findViewById(button_periodicos_section);
-        buttonPeriodicos.setOnClickListener(this);
-        Button buttonConferencias = findViewById(button_conferencias_section);
-        buttonConferencias.setOnClickListener(this);
-        Button buttonCorrelacao = findViewById(button_correlacao_section);
-        buttonCorrelacao.setOnClickListener(this);
-    }
+        buttonPeriodicos.setOnClickListener(view -> startActivity(
+                new Intent(PeriodicoSection.this, PeriodicoSection.class))
+        );
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View view){
-        switch (view.getId()){
-            case button_periodicos_section:
-                Intent periodicosIntent = new Intent(this, PeriodicoSection.class);
-                startActivity(periodicosIntent);
-            case button_conferencias_section:
-                Intent conferenciasIntent = new Intent(this, ConferenciaSection.class);
-                startActivity(conferenciasIntent);
-            case button_correlacao_section:
-                Intent correlacaoIntent = new Intent(this, ConferenciaSection.class);
-                startActivity(correlacaoIntent);
-        }
+        Button buttonConferencias = findViewById(button_conferencias_section);
+        buttonConferencias.setOnClickListener(view -> startActivity(
+                new Intent(PeriodicoSection.this, ConferenciaSection.class))
+        );
+
+        Button buttonCorrelacao = findViewById(button_correlacao_section);
+        buttonCorrelacao.setOnClickListener(view -> startActivity(
+                new Intent(PeriodicoSection.this, ConferenciaSection.class))
+        );
     }
 }
