9373b695b1c883d4f8fa3f9a093444eecf35eaec
SousaPedroso <gsousa844@gmail.com>
2022-12-08 11:29:22 -0400

    Finished sections base layout

--- app/src/main/java/com/example/qualis/ConferenciaSection.java
+++ app/src/main/java/com/example/qualis/ConferenciaSection.java
@@ -1,8 +1,14 @@
 package com.example.qualis;
 
+import static com.example.qualis.R.id.button_conferencias_section;
+import static com.example.qualis.R.id.button_correlacao_section;
+import static com.example.qualis.R.id.button_periodicos_section;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ConferenciaSection extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class ConferenciaSection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conferencia_section);
+
+        Button buttonPeriodicos = findViewById(button_periodicos_section);
+        buttonPeriodicos.setOnClickListener(view -> startActivity(
+                new Intent(ConferenciaSection.this, PeriodicoSection.class))
+        );
+
+        Button buttonConferencias = findViewById(button_conferencias_section);
+        buttonConferencias.setOnClickListener(view -> startActivity(
+                new Intent(ConferenciaSection.this, ConferenciaSection.class))
+        );
+
+        Button buttonCorrelacao = findViewById(button_correlacao_section);
+        buttonCorrelacao.setOnClickListener(view -> startActivity(
+                new Intent(ConferenciaSection.this, ConferenciaSection.class))
+        );
     }
 }
