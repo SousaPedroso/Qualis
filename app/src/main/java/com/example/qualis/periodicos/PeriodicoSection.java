58b188500a383a5e5667d142888f4b1772b130e7
SousaPedroso <gsousa844@gmail.com>
2022-12-10 20:07:39 -0400

    Initial recycler view for Periodicos

--- app/src/main/java/com/example/qualis/periodicos/PeriodicoSection.java
+++ app/src/main/java/com/example/qualis/periodicos/PeriodicoSection.java
@@ -6,14 +6,19 @@ import static com.example.qualis.R.id.button_periodicos_section;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.qualis.ConferenciaSection;
+import com.example.qualis.R;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import com.example.qualis.ConferenciaSection;
-import com.example.qualis.Correlacao_Section;
-import com.example.qualis.R;
 
 public class PeriodicoSection extends AppCompatActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +36,19 @@ public class PeriodicoSection extends AppCompatActivity{
 
         Button buttonCorrelacao = findViewById(button_correlacao_section);
         buttonCorrelacao.setOnClickListener(view -> startActivity(
-                new Intent(PeriodicoSection.this, Correlacao_Section.class))
+                new Intent(PeriodicoSection.this, ConferenciaSection.class))
         );
+
+        RecyclerView recyclerView = findViewById(R.id.periodicos_recycler_view);
+        final PeriodicoListAdapter adapter = new PeriodicoListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        PeriodicoViewModel periodicoViewModel = new ViewModelProvider(this).get(PeriodicoViewModel.class);
+        periodicoViewModel.getAllPeriodicos().observe(this, words -> {
+            // Update the cached copy of the words in the adapter.
+            adapter.setPeriodicos(words);
+        });
+
     }
 }
