06d507b17625cddc887e14a68fa13de6faec78f4
Guilherme George <72107163+Just-GGeorge@users.noreply.github.com>
2022-12-12 14:46:03 -0400

    Implementando a busca e os filtro capes
    Implementação do search view e spinner, utilizados nos metodos para pesquisar e filtrar por categoria

--- app/src/main/java/com/example/qualis/periodicos/PeriodicoSection.java
+++ app/src/main/java/com/example/qualis/periodicos/PeriodicoSection.java
@@ -6,6 +6,7 @@ import static com.example.qualis.R.id.button_periodicos_section;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,7 +16,12 @@ import com.example.qualis.R;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class PeriodicoSection extends AppCompatActivity{
 
@@ -49,6 +55,68 @@ public class PeriodicoSection extends AppCompatActivity{
             // Update the cached copy of the words in the adapter.
             adapter.setPeriodicos(words);
         });
+
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCategory = (String) parent.getItemAtPosition(position);
+
+                if (selectedCategory.equals("Todos")) {
+                    List<Periodico> allPeriodicos = periodicoViewModel.getAllPeriodicos().getValue();
+
+                    adapter.setPeriodicos(allPeriodicos);
+                } else {
+                    List<Periodico> filteredPeriodicos = periodicoViewModel.filterPeriodicosByCategory(selectedCategory);
+
+                    adapter.setPeriodicos(filteredPeriodicos);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+
+
+        SearchView searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle the search query here
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Filter the list of Periodico objects based on the search query
+                // Get the selected category and search query from the spinner and search view, respectively
+                String selectedCategory = (String) spinner.getSelectedItem();
+                String searchQuery = searchView.getQuery().toString();
+
+                // Filter the list of periodicos by the selected category and search query
+                List<Periodico> filteredPeriodicos = periodicoViewModel.searchAndFilterPeriodicos(searchQuery, selectedCategory);
+
+                // Update the list in the RecyclerView adapter with the filtered list
+                adapter.setPeriodicos(filteredPeriodicos);
+
+                // Check if the LiveData object is not null before accessing its data
+                // Use the post method of the RecyclerView object to update the adapter on the main thread
+                // after the background thread has finished filtering the list of periodicos
+                recyclerView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.setPeriodicos(filteredPeriodicos);
+                    }
+                });
+
+
+                return false;
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/qualis/periodicos/PeriodicoViewModel.java
+++ app/src/main/java/com/example/qualis/periodicos/PeriodicoViewModel.java
@@ -5,13 +5,14 @@ import android.app.Application;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PeriodicoViewModel extends AndroidViewModel {
 
     private PeriodicoRepository mRepository;
 
-    private LiveData<List<Periodico>> mAllPeriodicos;
+    private static LiveData<List<Periodico>> mAllPeriodicos;
 
     public PeriodicoViewModel (Application application) {
         super(application);
@@ -19,7 +20,52 @@ public class PeriodicoViewModel extends AndroidViewModel {
         mAllPeriodicos = mRepository.getAllPeriodicos();
     }
 
+    public List<Periodico> filterPeriodicosByCategory(String category) {
+        List<Periodico> filteredPeriodicos = new ArrayList<>();
+
+        List<Periodico> allPeriodicos = mAllPeriodicos.getValue();
+
+        if (allPeriodicos != null) {
+            for (Periodico periodico : allPeriodicos) {
+                if (periodico.getExtratoCapes().equals(category)) {
+                    filteredPeriodicos.add(periodico);
+                }
+            }
+        }
+
+        return filteredPeriodicos;
+    }
+
+
+    public List<Periodico> searchAndFilterPeriodicos(String query, String category) {
+        List<Periodico> filteredPeriodicos = new ArrayList<>();
+
+        List<Periodico> allPeriodicos = getAllPeriodicos().getValue();
+
+        if (!category.equals("Todos")) {
+            allPeriodicos = filterPeriodicosByCategory(category);
+        }
+
+
+
+        query = query.toLowerCase();
+        for (int i = 0; i < allPeriodicos.size(); i++) {
+            Periodico periodico = allPeriodicos.get(i);
+            String periodicoName = periodico.getNome().toLowerCase();
+
+            if (periodicoName.contains(query)) {
+                filteredPeriodicos.add(periodico);
+            }
+        }
+
+        return filteredPeriodicos;
+    }
+
     LiveData<List<Periodico>> getAllPeriodicos() { return mAllPeriodicos; }
 
     public void insert(Periodico periodico) { mRepository.insert(periodico); }
+
+
+
+
 }
